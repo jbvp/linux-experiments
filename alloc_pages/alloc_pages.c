665d41abec423f9bccff586ff6607d901efcee99
JB Van Puyvelde <jbvanpuyvelde@gmail.com>
2017-06-08 00:19:08 +0200

    alloc_pages: fix the pointer passed to __free_pages()
    Since the for loop incremented the original page pointer, __free_pages()
    was called with an erroneous pointer.
    
    Adds another pointer to be used as an iterator inside the for loop.

--- alloc_pages/alloc_pages.c
+++ alloc_pages/alloc_pages.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/gfp.h>
 #include <linux/mm.h>
+#include <linux/kernel.h>
 
 #define AP_ORDER 1
 static struct page *page;
@@ -9,14 +10,16 @@ static struct page *page;
 static int __init ap_init(void)
 {
 	unsigned int i;
+	struct page *p;
 
+	pr_debug("Alloc %d contiguous pages with alloc_pages\n", 1 << AP_ORDER);
 	page = alloc_pages(GFP_KERNEL, AP_ORDER);
 	if (!page)
 		return -ENOMEM;
 
-	for (i = 0; i < 2; i++) {
-		pr_debug("page_address = %p\n", page_address(page));
-		page += sizeof(struct page);
+	for (i = 0, p = page; i < (1 << AP_ORDER); i++, p++) {
+		pr_debug("page_address (virtual address) = %p\n",
+				page_address(page));
 	}
 
 	return 0;
